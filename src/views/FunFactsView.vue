7bb132241cee7692798f31db0ca5efffcd651727
DeJoon <jandein04@gmail.com>
2023-09-19 20:15:20 +0200

    Refactored all texts to make them grammatically more accurate.

--- src/views/FunFactsView.vue
+++ src/views/FunFactsView.vue
@@ -6,7 +6,7 @@ const funFacts = ref([
   {icon: ['fas', 'cat'], amount: 1000, text: 'Katzen Videos geschaut'},
   {icon: ['far', 'eye'], amount: 220, text: 'Anime abgeschlossen'},
   {icon: ['fas', 'book'], amount: 150, text: 'Manga im Besitz'},
-  {icon: ['fas', 'language'], amount: 1, text: 'Japanisch Units in Duolingo gelernt'},
+  {icon: ['fas', 'language'], amount: 5, text: 'Japanisch Units in Duolingo gelernt'},
 ])
 </script>
 
