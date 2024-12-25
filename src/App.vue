32364c64dadbeb9f5c2b9ea9bec55a4904705c46
dejoon <jandein04+github@gmail.com>
2024-12-25 20:13:17 +0100

    Added eslint rules to enforce a certain style for the whole project. Additionally moved the language switcher to it's own component. The switcher will stick to the top on small screens but will follow on bigger screens.

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,17 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useI18n } from 'vue-i18n';
-import { useRoute, useRouter } from 'vue-router';
 import AboutMeView from '@/views/AboutMeView.vue';
 import ExperienceView from '@/views/ExperienceView.vue';
 import FindMeOnView from '@/views/FindMeOnView.vue';
 import FunFactsView from '@/views/FunFactsView.vue';
 import IntroductionView from '@/views/IntroductionView.vue';
+import LanguageSwitcher from '@/views/LanguageSwitcher.vue';
 import LanguageView from '@/views/LanguageView.vue';
 import ProjectsView from '@/views/ProjectsView.vue';
 import ResumeView from '@/views/ResumeView.vue';
-
-const { locale, availableLocales, fallbackLocale } = useI18n({ useScope: 'global' });
-const route = useRoute();
-const router = useRouter();
-const selectedLocale = ref(locale.value);
-
-const updateLocale = () => {
-  const newLocale = availableLocales.includes(selectedLocale.value) ? selectedLocale.value : fallbackLocale.value;
-  locale.value = newLocale;
-  selectedLocale.value = newLocale;
-  router.push({ query: { locale: newLocale } });
-};
-
-watch(
-  () => route.query,
-  (query) => {
-    selectedLocale.value = query.locale;
-    updateLocale();
-  },
-  { immediate: true }
-);
 </script>
 
 <template>
-  <div class="position-fixed top-0 end-0 p-3 z-3">
-    <select
-      v-model="selectedLocale"
-      class="form-select"
-      aria-label="Default select example"
-      @change="updateLocale"
-    >
-      <option value="de">
-        Deutsch
-      </option>
-      <option value="en">
-        English
-      </option>
-    </select>
-  </div>
+  <LanguageSwitcher />
 
   <IntroductionView />
 
---
+++ src/views/LanguageSwitcher.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useRoute, useRouter } from 'vue-router';
+
+const { locale, availableLocales, fallbackLocale } = useI18n({ useScope: 'global' });
+const route = useRoute();
+const router = useRouter();
+const selectedLocale = ref(locale.value);
+
+const updateLocale = () => {
+  const newLocale = availableLocales.includes(selectedLocale.value) ? selectedLocale.value : fallbackLocale.value;
+  locale.value = newLocale;
+  selectedLocale.value = newLocale;
+  router.push({ query: { locale: newLocale } });
+};
+
+watch(
+  () => route.query,
+  (query) => {
+    const queryLocale = query.locale;
+
+    if (undefined === queryLocale) {
+      return;
+    }
+
+    selectedLocale.value = queryLocale;
+    updateLocale();
+  },
+  { immediate: true }
+);
+</script>
+
+<template>
+  <div class="select-wrapper top-0 end-0 p-3 z-3">
+    <select
+      v-model="selectedLocale"
+      class="form-select"
+      aria-label="Default select example"
+      @change="updateLocale"
+    >
+      <option value="de">
+        Deutsch
+      </option>
+      <option value="en">
+        English
+      </option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+@media (max-width: 1226px) {
+  .select-wrapper {
+    position: absolute;
+  }
+}
+
+@media (min-width: 1226px) {
+  .select-wrapper {
+    position: fixed;
+  }
+}
+</style>
