331b1225ca6e5637685d7a5a5ca451022d96a347
dejoon <jandein04+github@gmail.com>
2024-12-26 01:30:01 +0100

    Open Graph protocol test

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { createHead } from '@unhead/vue';
 import { createApp } from 'vue';
 import App from './App.vue';
 import meImage from './assets/img/me.jpg';
@@ -16,21 +15,12 @@ import router from './router';
 
 config.autoAddCss = false;
 library.add(fas, fab, far);
-const head = createHead();
-head.push({
-  title: 'Home - My Vue App',
-  meta: [
-    { property: 'og:title', content: 'Portfolio - Jan Lukas Dein' },
-    { property: 'og:description', content: 'This is the portfolio of Jan Lukas Dein. If you want to know more about my skills give it a visit.' },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: window.location.origin },
-    { property: 'og:image', content: `${window.location.origin}${meImage}` },
-  ],
-});
+
+const head = document.head;
+head.innerHTML = head.innerHTML.replace('%url%', window.location.origin).replace('%image_url%', `${window.location.origin}${meImage}`);
 
 createApp(App)
   .use(i18n)
-  .use(head)
   .use(router)
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
