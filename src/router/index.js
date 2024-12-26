8012e6afe9af95a49f1b19a077cbff2a13403950
DeJoon <66092923+DeJoon@users.noreply.github.com>
2024-12-26 01:35:38 +0100

    Test og (#3)
    Added basic Open Graph tags

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import AppView from '../App.vue';
+import HomeView from '@/views/HomeView.vue';
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
-    { path: '/', component: AppView },
+    { path: '/', component: HomeView },
   ],
 });
 
